711456999e1533be4818327ec5cb9040b79df9d2
Edwix01 <edwinlima201@gmail.com>
2024-09-28 16:17:23 -0500

    Actualización script bridge_id.py

--- collector/bridge_id.py
+++ collector/bridge_id.py
@@ -12,14 +12,27 @@ def bri_id(ips,datos):
             cmdgen.CommunityData(comunidad),
             cmdgen.UdpTransportTarget((server_ip, 161)),
             0,25,
+            #Despliegue
+            #'1.3.6.1.2.1.17.1.1'
+
+            #Simulación
             '1.3.6.1.2.1.2.2.1.6'
         )
         if errorIndication != None:
             f = 1
             fif[server_ip] = ""
         
+
+        #Código para simulación
         for varBindTableRow in varBindTable:
             for name, val in varBindTableRow:
                 if  (name.prettyPrint()).split('.')[-1] == "1":
                     a[server_ip] = val.prettyPrint()[2::]
+
+        #Código para despliegue
+        """
+        for varBindTableRow in varBindTable:
+            for name, val in varBindTableRow:
+                    a[server_ip] = val.prettyPrint()[2::]
+        """
     return a,f,fif
